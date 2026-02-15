fa90fb9d0f4a8c9e46b8c2a5159f48a8058ea660
pparnfaa <pparnfaa@odds.team>
2026-02-15 22:01:07 +0700

    💄 style: implement follow ui in session page

--- app/helpers/checkin_forms_helper.rb
+++ app/helpers/checkin_forms_helper.rb
@@ -1,2 +1,10 @@
 module CheckinFormsHelper
+  def attendance_count_label(count)
+    number_class = count > 0 ? "text-[#261ce6]" : "text-[#6B7280]"
+    label = count > 1 ? "Students" : "Student"
+
+    content_tag(:span, class: "text-xs font-semibold text-[#6B7280]") do
+      content_tag(:span, count, class: number_class) + " #{label}"
+    end
+  end
 end
--- config/application.rb
+++ config/application.rb
@@ -23,5 +23,8 @@ module CheckinManagement
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+
+    config.time_zone = "Bangkok"
+    config.active_record.default_timezone = :local
   end
 end
